177e95530cb22aa2979a6051040520efd7f65452
Arif <arifkutq63@gmail.com>
2023-01-11 17:40:13 +0200

    work

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,14 @@
-import "./LoginForm.module.css"
-export default function LoginForm() {
-return (
-  <div className="form">
-    <form>
-      <label className="label">
-        <input className="input" type="text" placeholder="username"/>
-        <input className="input" type="password" placeholder="password"/>
-        <button className="submit" type="submit">Submit</button>
-      </label>
-    </form>
+import "./LoginForm.module.css";
 
-  </div>
-)
+export default function LoginForm() {
+  return (
+    <div><label className="label">
+      <form className="form">
+        <input className="input" type="text" placeholder="username" />
+        <input className="input" type="password" placeholder="password" />
+        <button className="submit" type="submit">Submit</button>
+      </form>
+    </label>
+    </div>
+  );
 }
